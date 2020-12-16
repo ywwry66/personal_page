3a202f29fd6fa260bcae467f9493e184b7190722
Ruiyang Wu <58066925+ywwry66@users.noreply.github.com>
2020-12-15 18:35:52 -0700

    Initial commit

---
+++ publish.el
@@ -0,0 +1,36 @@
+(require 'org)
+(require 'ox-publish)
+
+(setq my-site-extra-head "<link rel='stylesheet' href='static/worg.css' />")
+(setq org-publish-project-alist
+      '(("blog"
+         :components ("blog-content" "blog-static"))
+        ("blog-content"
+         :base-directory "./"
+         :base-extension "org"
+         :publishing-directory "./public"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :export-with-tags nil
+         :headline-levels 4             ; Just the default for this project.
+         :table-of-contents nil
+         :section-numbers nil
+         :sub-superscript nil
+         :todo-keywords nil
+         :author nil
+         :creator-info nil
+	 :html-head nil
+	 :html-head-extra "<link rel='stylesheet' href='static/worg.css' />"
+         :html-preamble nil
+         :html-postamble "<hrstyle=\"text-align: center;position: fixed;left: 0;bottom: 0;\">\n
+<p style=\"text-align: center;position: fixed;left: 0;bottom: 0;\"> Made with org-mode </p>"
+         :style "This is raw html for stylesheet <link>'s"
+         :timestamp t
+         :exclude-tags ("noexport" "todo")
+         :auto-preamble t)
+        ("blog-static"
+         :base-directory "./static"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|otf\\|jpeg"
+         :publishing-directory "./public/static"
+         :recursive t
+         :publishing-function org-publish-attachment)))
