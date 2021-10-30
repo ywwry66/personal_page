83de530c4de3a306ee189acb857eb99182eaccac
Ruiyang Wu <58066925+ywwry66@users.noreply.github.com>
2021-10-30 15:56:58 -0700

    Comment unused lines, add preamble

--- publish.el
+++ publish.el
@@ -1,7 +1,7 @@
 (require 'org)
 (require 'ox-publish)
 
-(setq my-site-extra-head "<link rel='stylesheet' href='static/worg.css' />")
+;; (setq my-site-extra-head "<link rel='stylesheet' href='static/worg.css' />")
 (setq org-publish-project-alist
       '(("blog"
          :components ("blog-content" "blog-static"))
@@ -12,20 +12,20 @@
          :publishing-directory "./public"
          :recursive t
          :publishing-function org-html-publish-to-html
-         :export-with-tags nil
+	 
+         ;; :export-with-tags nil
          :headline-levels 4             ; Just the default for this project.
-         :table-of-contents nil
+         :with-toc nil
          :section-numbers nil
-         :sub-superscript nil
-         :todo-keywords nil
-         :author nil
-         :creator-info nil
-	 :html-head nil
-	 :html-head-extra "<link rel='stylesheet' href='static/worg.css' />"
-         :html-preamble nil
+         ;; :sub-superscript nil
+         ;; :todo-keywords nil
+         ;; :author nil
+         ;; :creator-info nil
+	 :html-head "<link rel='stylesheet' href='static/worg.css' />"
+         :html-preamble "<a href=\"index.html\">Home</a> <a href=\"teaching.html\">Teaching</a> <a href=\"research.html\">Research</a>"
          :html-postamble "<hrstyle=\"text-align: center;position: fixed;left: 0;bottom: 0;\">\n
 <p style=\"text-align: center;position: fixed;left: 0;bottom: 0;\"> Made with org-mode </p>"
-         :style "This is raw html for stylesheet <link>'s"
+         ;; :style "This is raw html for stylesheet <link>'s"
          :timestamp t
          :exclude-tags ("noexport" "todo")
          :auto-preamble t)
